f73d8a1ae178165944e2b9a192689aa92a0e1eea
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-03 22:49:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 	<div class="btn-group-vertical">
     	<input type=button onClick="location.href='calculate.php'" class="btn" value="CALCULATE BMI">
-    	<input type=button onClick="location.href='http://localhost/BMI/api/progress_update.html'" class="btn" value="PERFORMANCE">
+    	<input type=button onClick="location.href='progress_update.html'" class="btn" value="PERFORMANCE">
     	<input type=button onClick="location.href='advice.php'" class="btn" value="NUTRITIONAL ADVICE">
 	</div>
 
