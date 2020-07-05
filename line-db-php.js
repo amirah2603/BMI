f8e89b029476fb300db7d99621adbbc9f864c013
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-05 15:43:54 +0800

    Update line-db-php.js

--- line-db-php.js
+++ line-db-php.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
    * call the data.php file to fetch the result from db table.
    */
   $.ajax({
-    url : "http://34.92.199.218/line-db-php.js",
+    url : "http://34.92.199.218/bmi.php",
     type : "GET",
     success : function(data) {
       console.log(data);
