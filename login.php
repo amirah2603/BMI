75e351377babd71d119a17168f11ffc814b06d11
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-07 00:05:10 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+	<div class="btn-group-vertical">
+		<h1>Login As: </h1>
+		<button onclick="location.href='home.php?user=amirah'" type="submit"  class="btn" >AMIRAH</button>
+		<button onclick="location.href='home.php?user=amirah'" class="btn"  >GUEST</button>
+		<!-- <input type="submit"  class="btn" name="user" value="GUEST" > -->
+
+		<!-- <h1>Login As: </h1>
+    	<input type=button onClick="location.href='home1.html'" class="btn" value="AMIRAH">
+    	<input type=button onClick="location.href='home2.html'" class="btn" value="GUEST"> -->
+	</div>
+
+</body>
+</html>
