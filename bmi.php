56088e0dcbd1f83af0fa2be27f6d3710deabf13d
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-06 12:36:19 +0800

    Update bmi.php

--- bmi.php
+++ bmi.php
@@ -28,25 +28,27 @@ if(!$mysqli){
   die("Connection failed: " . $mysqli->error);
 }
 
+echo "Database connected";
+
 //query to get data from the table
 $query = sprintf("SELECT id, user_bmi, created_at FROM bmi ");
 
 //execute query
 $result = $mysqli->query($query);
 
-//loop through the returned data
-$data = array();
-foreach ($result as $row) {
-  $data[] = $row;
-}
+// //loop through the returned data
+// $data = array();
+// foreach ($result as $row) {
+//   $data[] = $row;
+// }
 
-//free memory associated with result
-$result->close();
+// //free memory associated with result
+// $result->close();
 
 //close connection
 $mysqli->close();
 
 //now print the data
-print json_encode($data);
+print json_encode($result);
 
 ?>
