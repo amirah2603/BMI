9e9675617b210cd28d610f45ada5be6bb56637bb
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-07 10:44:54 +0800

    Update progress_update.php

--- progress_update.php
+++ progress_update.php
@@ -42,14 +42,14 @@
   <!-- javascript -->
     <script type="text/javascript" src="jquery.min.js"></script>
     <script type="text/javascript" src="Chart.min.js"></script>
-    <?php $user = $_GET['user'];
+<!--     <?php $user = $_GET['user'];
 	if($user == 'amirah'){
 	    <script type="text/javascript" src="line-db-php(amirah).js"></script>
 	}
 	else{
 	   <script type="text/javascript" src="line-db-php(guest).js"></script>
 	}
-   ?>
+   ?> -->
   <div class="performance-group-vertical">
     <input type=button onClick="location.href='home.php?user=<?php echo $user?>'" class="performance" value="HOME">
   </div>
