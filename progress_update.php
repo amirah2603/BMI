30b2be6a75ae8c304da3463dcbae8b70928e8e47
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-07 10:47:52 +0800

    Update progress_update.php

--- progress_update.php
+++ progress_update.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<!--     <meta content="text/html;charset=utf-8" http-equiv="Content-Type"> -->
     <meta content="utf-8" http-equiv="encoding">
     <title>Performance</title>
     <link href="default.css" rel="stylesheet">
