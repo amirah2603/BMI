151a7a94b247e6e40dfd51122b86fee1612fc5df
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-07 11:26:32 +0800

    Create progress.php

---
+++ progress.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Creating Dynamic Data Graph using PHP and Chart.js</title>
+<style type="text/css">
+BODY {
+    width: 550PX;
+}
+
+#chart-container {
+    width: 100%;
+    height: auto;
+}
+</style>
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript" src="Chart.min.js"></script>
+
+
+</head>
+<body>
+    <div id="chart-container">
+        <canvas id="graphCanvas"></canvas>
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            showGraph();
+        });
+
+
+        function showGraph()
+        {
+            {
+                $.post("bmi.php",
+                function (data)
+                {
+                    console.log(data);
+                     var bmi = [];
+                     var date/time = [];
+
+                    for (var i in data) {
+                        name.push(data[i].user_bmi);
+                        marks.push(data[i].created_at);
+                    }
+
+                    var chartdata = {
+                        labels: name,
+                        datasets: [
+                            {
+                                label: 'BMI',
+                                backgroundColor: '#49e2ff',
+                                borderColor: '#46d5f1',
+                                hoverBackgroundColor: '#CCCCCC',
+                                hoverBorderColor: '#666666',
+                                data: marks
+                            }
+                        ]
+                    };
+
+                    var graphTarget = $("#graphCanvas");
+
+                    var lineGraph = new Chart(graphTarget, {
+                        type: 'line',
+                        data: chartdata
+                    });
+                });
+            }
+        }
+        </script>
+
+</body>
+</html
