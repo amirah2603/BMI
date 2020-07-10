0768ec33d009e3c8afcd8da92dc562c1e2ba26a7
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-10 13:04:52 +0800

    Update progress_update.php

--- progress_update.php
+++ progress_update.php
@@ -45,83 +45,15 @@
   <div class="chart-container">
     <canvas id="line-chartcanvas"></canvas>
   </div>   
-  <!-- javascript -->
-<!--     <script type="text/javascript" src="jquery.min.js"></script>
-    <script type="text/javascript" src="Chart.min.js"></script>
--->
-   <script type="text/javascript">
-  $(document).ready(function() {
-
-  /**
-   * call the data.php file to fetch the result from db table.
-   */
-  $.ajax({
-    url : "https://34.92.199.218/bmi.php",
-    type : "GET",
-    success : function(data) {
-      console.log(data);
-
-      // var score = {
-      //   TeamA : [],
-      //   TeamB : []
-      // };
-      var bmi = [];
-
-      var date = [];
-
-
-      var len = data.length;
-
-      for (var i = 0; i < len; i++){
-        bmi.push(data[i].user_bmi);
-        date.push(data[i].created_at);
-      }
-
-      console.log(bmi)
-      console.log(date)
-
-
-      var ctx = $("#line-chartcanvas");
-
-      var data = {
-        labels : date,
-        datasets : [
-          {
-            label : "Your BMI",
-            data : bmi,
-            backgroundColor : "blue",
-            borderColor : "lightblue",
-            fill : false,
-            LineTension : 0,
-            pointRadius : 5
-          },
-          // {
-          //   label : "TeamB score",
-          //   data : score.TeamB,
-          //   backgroundColor : "blue",
-          //   borderColor : "lightblue",
-          //   fill : false,
-          //   LineTension : 0,
-          //   pointRadius : 5
-          // }
-        ]
-      };
-
-      var chart = new Chart( ctx, {
-        type : "line",
-        data : data,
-        options : {}
-      });
-
-    },
-    error : function(data){
-      console.log(data);
-    }
-
-  });
-
-});
-  </script>
+<php? 
+  $user = $_GET['user'];
+  if($user == "amirah"){
+      <script type="text/javascript" src="line-db-php.js(amirah)"></script>
+  }
+  else{
+	<script type="text/javascript" src="line-db-php.js(guest)"></script>
+  }
+?>
   <div class="performance-group-vertical">
     <input type=button onClick="location.href='home.php?user=<?php echo $user?>'" class="performance" value="HOME">
   </div>
