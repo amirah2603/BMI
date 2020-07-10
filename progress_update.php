725208757fe635e102a4b0af9ec98d3f56b1911e
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-10 13:24:59 +0800

    Update progress_update.php

--- progress_update.php
+++ progress_update.php
@@ -44,63 +44,7 @@
   <div class="chart-container">
     <canvas id="line-chartcanvas"></canvas>
   </div>   
-<?php 
-// 	$user = $_GET['user'];
-	if(GET['user'] == "amirah"){
-		<script type="text/javascript">
-  $(document).ready(function() {
-  $.ajax({
-    url : "https://34.92.199.218/bmi.php",
-    type : "GET",
-    success : function(data) {
-      console.log(data);
-      var bmi = [];
-      var date = [];
-      var len = data.length;
-      for (var i = 0; i < len; i++){
-        bmi.push(data[i].user_bmi);
-        date.push(data[i].created_at);
-      }
-
-      console.log(bmi)
-      console.log(date)
-      var ctx = $("#line-chartcanvas");
-      var data = {
-        labels : date,
-        datasets : [
-          {
-            label : "Your BMI",
-            data : bmi,
-            backgroundColor : "blue",
-            borderColor : "lightblue",
-            fill : false,
-            LineTension : 0,
-            pointRadius : 5
-          },
-        ]
-      };
-
-      var chart = new Chart( ctx, {
-        type : "line",
-        data : data,
-        options : {}
-      });
-
-    },
-    error : function(data){
-      console.log(data);
-    }
-
-  });
-
-});
-  </script>
-}
-else{
-	echo "no option";
-}
-?>
-
+<script type="text/javascript" src="line-db-php.js"></script>
   <div class="performance-group-vertical">
     <input type=button onClick="location.href='home.php?user=<?php echo $user?>'" class="performance" value="HOME">
   </div>
