e8a585c3a14b040d88c51228c01cf8af74ec0898
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-08-05 22:13:59 +0800

    Update advice.php

--- advice.php
+++ advice.php
@@ -22,7 +22,12 @@
   		<div class="col my-col">
   			<div class="centered">
   				<?php
-				$url = "http://34.92.199.218/bmi.php";
+				if (user=="amirah"){
+					$url = "http://34.92.199.218/bmi.php";
+				}
+				else{
+					$url = "http://34.92.199.218/bmi_guest.php";
+				}
     				$json = json_decode(file_get_contents($url), true);
 				$lastValue = end($json);
 				$BMI = $lastValue['user_bmi'];
