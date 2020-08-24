0aa1e42dd22794163772ae99542f7c82261c2ffb
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-08-24 22:56:36 +0800

    Update advice.php

--- advice.php
+++ advice.php
@@ -22,7 +22,7 @@
   		<div class="col my-col">
   			<div class="centered">
   				<?php
-				if (user=="guest"){
+				if ($user=="guest"){
 					$url = "http://34.92.199.218/bmi_guest.php";
 				}
 				else{
